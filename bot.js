af68436fa7545da74c8bda12b8fefa3b6fada7d0
Rasmita Jena <rasmita.jena@ad.infosys.com>
2020-07-06 13:43:01 +0530

    Finish anyhow2

--- bot.js
+++ bot.js
@@ -32,10 +32,13 @@ class TeamsConversationBot extends TeamsActivityHandler {
                
             
                const channels = await TeamsInfo.getTeamChannels(context,teamId);
+            
+            
                
+               const cahnel_details=channels.
                var channeldetails='';
                channels.forEach(element => {
-                channeldetails+=element;
+                channeldetails+=element.id;
                });
 
              
