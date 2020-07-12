0890258084e8a377e88db0a6a574c161b53fdaa5
Rasmita Jena <rasmita.jena@ad.infosys.com>
2020-07-12 20:05:48 +0530

    final adaptive card changes

--- bot.js
+++ bot.js
@@ -14,6 +14,11 @@ const {
     var _=require('underscore');
  
 const TextEncoder = require('util').TextEncoder;
+const RestaurantCard = require('./resources/adaptiveCards/Restaurantcard.json');
+const CARDS = [
+
+    RestaurantCard
+];
 
 
 const { CreateIncidentDialogue } = require('./CreateIncidentDialogue.js')
@@ -109,33 +114,7 @@ this.onMembersAdded(async (context, next) => {
         await this.userState.saveChanges(context);
     }
 
-    async sendIntroCard(context) {
-        const card = CardFactory.heroCard(
-            'Welcome to Bot Framework!',
-            'Welcome to Welcome Users bot sample! This Introduction card is a great way to introduce your Bot to the user and suggest some things to get them started. We use this opportunity to recommend a few next steps for learning more creating and deploying bots.',
-            ['https://aka.ms/bf-welcome-card-image'],
-            [
-                {
-                    type: ActionTypes.OpenUrl,
-                    title: 'Get an overview',
-                    value: 'https://docs.microsoft.com/en-us/azure/bot-service/?view=azure-bot-service-4.0'
-                },
-                {
-                    type: ActionTypes.OpenUrl,
-                    title: 'Create Incident',
-                    value: 'https://stackoverflow.com/questions/tagged/botframework'
-                },
-                {
-                    type: ActionTypes.OpenUrl,
-                    title: 'Update Incident',
-                    value: 'https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-howto-deploy-azure?view=azure-bot-service-4.0'
-                }
-            ]
-        );
-    
-        await context.sendActivity({ attachments: [card] });
-    }
-
+   
 
     async dispatchToIntentAsync(context){
 
@@ -235,8 +214,13 @@ this.onMembersAdded(async (context, next) => {
 
 
     async sendSuggestedActions(turnContext) {
-        var reply = MessageFactory.suggestedActions(['Update Incident','Check IncidentHistory','Create Incident'],'What would you like to do today ?');
-        await turnContext.sendActivity(reply);
+       
+
+        
+        await turnContext.sendActivity({
+            text: 'What would you like to do today ?',
+            attachments: [CardFactory.adaptiveCard(CARDS[0])]
+        });
     }
 
     async sendSuggestedActionsinEndDialogue(turnContext) {
